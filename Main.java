f4316023a4286494fbafefa6795c78d8238e2cf4
xiaoyangma <xiaoyangma@tencent.com>
2020-07-30 14:12:53 +0800

    add Pair

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 public class Main {
-    public static void main (String [] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Set<Pair<String, String>> set = new HashSet<>();
+
+        Pair<String, String> p1 = new Pair("1","3");
+        Pair<String, String> p2 = new Pair("1","3");
+
+        System.out.println(p1.equals(p2));
     }
 }
---
+++ Pair.java
@@ -0,0 +1,49 @@
+public class Pair<K extends Comparable<K>, V extends Comparable<V>> implements Comparable<Pair<K, V>> {
+
+    private K key;
+
+    public K getKey() { return key; }
+
+    private V value;
+
+    public V getValue() { return value; }
+
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode() * 13 + (value == null ? 0 : value.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof Pair) {
+            Pair pair = (Pair) o;
+            if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
+            if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
+            return true;
+        }
+        return false;
+     }
+
+     @SuppressWarnings("unchecked")
+     private int compare(Object a, Object b) {
+        return a == null ? (b == null ? 0 : -1) : (b == null) ? 1 :  ((Comparable<Object>) a).compareTo(b);
+     }
+
+     @Override
+     public int compareTo(Pair<K, V> pair) {
+        int c = compare(key, pair.getKey());
+        return c == 0 ? compare(value, pair.getValue()) : c;
+     }
+}
