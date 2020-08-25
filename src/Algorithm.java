f823234a944ce8e875e3d2b4afa3bd8519353290
maxiaoyang <maxiaoyang@tencent.com>
2020-08-25 23:27:58 +0800

    add algorithm

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,6 +1,6 @@
 package src;
 
-public class Algorithm {
+class Algorithm {
     void nextPermutation(int[] nums) {
         int maxIdx = nums.length - 1;
         int i = maxIdx;
@@ -24,4 +24,22 @@ public class Algorithm {
             nums[i + maxIdx - k] = temp;
         }
     }
+
+    //Arrays.binarySearch(arr, target)
+    //Collections.binarySearch(Arrays.stream(arr).boxed().collect(Collectors.toList()), target)
+    int binarySearch(int[] arr, int target) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (arr[m] < target) {
+                l = m + 1;
+            } else if (arr[m] > target) {
+                r = m - 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 package src;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        Tree t = new Tree();
-        String s = "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]";
-        TreeNode root = t.constructTree(s);
-        System.out.println(t.preOrder(root).toString());
-        System.out.println(t.inOrder(root).toString());
-        System.out.println(t.postOrder(root).toString());
-        System.out.println(t.postOrder1(root).toString());
+        Algorithm algorithm = new Algorithm();
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+        System.out.println(algorithm.binarySearch(arr, target));
+        System.out.println(Arrays.binarySearch(arr, target));
+        System.out.println(Collections.binarySearch(Arrays.stream(arr).boxed().collect(Collectors.toList()), target));
     }
 }
