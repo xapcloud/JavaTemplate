79b2213fab33ef1f17ab8eab305040bdaa12dd76
maxiaoyang <maxiaoyang@tencent.com>
2021-12-10 01:03:18 +0800

    add BITree

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -4,8 +4,8 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 class UnionFind {
-    int n;
-    int[] parent;
+    private int n;
+    private int[] parent;
 
     UnionFind(int n) {
         this.n = n;
@@ -38,6 +38,80 @@ class UnionFind {
 
 }
 
+class BITree {
+    private int n;
+    private int[] BIT;
+
+    private int lowBit(int n) {
+        return n & (-n);
+    }
+
+    public BITree(int[] arr) {
+        n = arr.length;
+        BIT = new int[n + 1];
+        constructTree1(arr);
+    }
+
+    // O(nlogn)
+    private void constructTree(int[] arr) {
+        for (int i = 0; i < this.n; i++) {
+            update(i, arr[i]);
+        }
+    }
+
+    // O(n)
+    private void constructTree1(int[] arr) {
+        System.arraycopy(arr, 0, BIT, 1, arr.length);
+        // 由于i必定是i+lowBit(i)的子节点，所以直接累加就OK
+        // C_8(1000) = sum(0 ~ 8] = arr[8] + sum(6, 7] + sum(4, 6] + sum(0, 4]
+        // 即 (110, 111] + (100, 110] + (0, 100]
+        for (int i = 1; i < n + 1; i++) {
+            int parent = i + lowBit(i);
+            if (parent < n + 1) {
+                BIT[parent] += BIT[i];
+            }
+        }
+    }
+
+    // 该函数仅仅帮助理解constructTree1
+    // 即为什么i必定是i+lowBit(i)的子节点
+    public List<Integer> findChildren(int n) {
+        List<Integer> ret = new ArrayList<>();
+        if (n <= 0) {
+            return ret;
+        }
+        int begin = n - lowBit(n);
+        n--;
+        while (n != begin && n > 0) {
+            ret.add(n);
+            n -= lowBit(n);
+        }
+        return ret;
+    }
+
+    public void update(int idx, int value) {
+        idx += 1;
+        while (idx < n + 1) {
+            this.BIT[idx] += value;
+            idx += lowBit(idx);
+        }
+    }
+
+    public int query(int idx) {
+        idx += 1;
+        int ret = 0;
+        while (idx > 0) {
+            ret += this.BIT[idx];
+            idx -= lowBit(idx);
+        }
+        return ret;
+    }
+
+    public int query(int a, int b) {
+        return query(b) - query(a - 1);
+    }
+}
+
 
 public class Algorithm {
     void nextPermutation(int[] nums) {
@@ -67,13 +141,13 @@ public class Algorithm {
     int findInsertPosition(int[] arr, int l, int r, int target) {
         if (arr[l] >= target)
             return l;
-        if(arr[r] < target) {
+        if (arr[r] < target) {
             return target + 1;
         }
 
-        while(r - l > 1) {
+        while (r - l > 1) {
             int mid = (r + l) / 2;
-            if(arr[mid] >= target) {
+            if (arr[mid] >= target) {
                 r = mid;
             } else {
                 l = mid;
@@ -177,21 +251,21 @@ public class Algorithm {
     // 最长回文子序列
     public int getLPSeq(String s) {
         int n = s.length();
-        int [][] lp = new int[n][n];
+        int[][] lp = new int[n][n];
         for (int i = 0; i < n; i++) {
             lp[i][i] = 1;
         }
 
         for (int i = 1; i < n; i++) {
-            for (int j = 0; i+j < n; j++) {
-                if (s.charAt(j) == s.charAt(i+j)) {
-                    lp[j][i+j] = lp[j+1][i+j-1] + 2;
+            for (int j = 0; i + j < n; j++) {
+                if (s.charAt(j) == s.charAt(i + j)) {
+                    lp[j][i + j] = lp[j + 1][i + j - 1] + 2;
                 } else {
-                    lp[j][i+j] = Math.max(lp[j+1][i+j], lp[j][i+j-1]);
+                    lp[j][i + j] = Math.max(lp[j + 1][i + j], lp[j][i + j - 1]);
                 }
             }
         }
-        return lp[0][n-1];
+        return lp[0][n - 1];
     }
 
 
